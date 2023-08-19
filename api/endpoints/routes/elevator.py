8a4105b7e234bda1bc0415f53251915f47a9f6f3
bassam-alamin <bassamalchinsky96@gmail.com>
2023-08-19 12:28:03 +0300

    feat: Added endpoint for to find elevator position

--- api/endpoints/routes/elevator.py
+++ api/endpoints/routes/elevator.py
@@ -3,6 +3,11 @@ from fastapi import APIRouter
 router = APIRouter()
 
 
-@router.post(path="/call")
+@router.get(path="/call")
 def call_elevator():
     return "Elevator is coming"
+
+
+@router.get(path="/position")
+def call_elevator():
+    return "Elevator is at floor 2"
