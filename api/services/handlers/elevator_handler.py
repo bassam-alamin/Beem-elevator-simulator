5684b794a2a1d337f567e39822d89c2383372d52
bassam-alamin <bassamalchinsky96@gmail.com>
2023-08-20 21:00:50 +0300

    feat: Added error handling

--- api/services/handlers/elevator_handler.py
+++ api/services/handlers/elevator_handler.py
@@ -1,4 +1,8 @@
-from api.domain.elevator import elevators, ElevatorDirection
+from starlette import status
+from starlette.exceptions import HTTPException
+from starlette.responses import JSONResponse
+
+from api.domain.elevator import elevators, ElevatorDirection, ElevatorStates
 
 
 class ElevatorController:
@@ -13,7 +17,19 @@ class ElevatorController:
 
     # Function to move elevator
     def call_elevator(self, requested_by=None):
-        print(f"Elevator called by: {requested_by}")
+        if self.elevator is None:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Elevator of id {self.elevator_id} "
+                       f"does not exist."
+            )
+        if self.elevator.state != ElevatorStates.IDLE:
+            raise HTTPException(
+                status_code=404,
+                detail=f"Elevator of id {self.elevator_id} "
+                       f"is busy please call another one "
+                       f"or wait."
+            )
         self.elevator.current_floor = self.from_floor
         self.elevator.stop_floor_list.add(self.to_floor)
         if self.from_floor > self.to_floor:
@@ -22,7 +38,3 @@ class ElevatorController:
             self.elevator.direction = ElevatorDirection.UP
         elevators[f"elevator_{self.elevator_id}"] = self.elevator
         return self.elevator
-
-
-
-
